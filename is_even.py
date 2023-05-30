0339a12211375e8dee8a79149f543fde03a253ab
Thomas Frank <thomas.frank@geo.uu.se>
2023-05-30 13:38:02 +0200

    'is_even' add doc

---
+++ is_even.py
@@ -0,0 +1,21 @@
+def is_even(x):
+    """
+    Checks if input is an even integer
+
+    Returns True if input is an even integer
+    Returns False if input is not an even integer
+    Raises Error if input is not an integer
+    """
+
+
+
+
+
+
+
+
+
+
+
+
+assert is_even.__doc__ is not None
